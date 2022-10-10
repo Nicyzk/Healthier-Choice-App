27f9753953cdd055b8373a0274c88db6ce53038c
isjods <chanxujodi@gmail.com>
2022-10-10 22:26:02 +0800

    jodi

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import HomePage from './components/HomePage'
 import Overlay from './components/Overlay'
+import MyProfilePage from './components/MyProfilePage'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import EditProfile from './components/EditProfile';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,8 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="overlay" component={Overlay} />
         <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="MyProfilePage" component={MyProfilePage} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- frontend/components/Overlay.js
+++ frontend/components/Overlay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Image, TouchableWithoutFeedback } from 'react-native';
 
 const Overlay = ({ navigation }) => (
-    <TouchableWithoutFeedback onPress={() => navigation.navigate('HomePage')}>
+    <TouchableWithoutFeedback onPress={() => navigation.navigate('MyProfilePage')}>
       <View className="flex-1 items-center justify-center bg-white" >
         <Image className="h-96 w-96" source={require('../assets/logo.png')} />
       </View>
