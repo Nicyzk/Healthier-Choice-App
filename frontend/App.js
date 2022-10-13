620ee6fd0b827744a65bcfe0b6b5b3211d0f5f7b
ayushmandixit2000 <ayushmandixit2000@gmail.com>
2022-10-13 12:11:52 +0800

    added info page and fixed navbar

--- frontend/App.js
+++ frontend/App.js
@@ -3,12 +3,8 @@ import HomePage from './containers/HomePage'
 import Overlay from './components/Overlay'
 import MyProfilePage from './components/MyProfilePage'
 import MyLists from './containers/MyLists'
-import SugarPage from './containers/SugarPage'
-import InfoPage from './containers/InfoPage'
-import SodiumPage from './containers/SodiumPage'
-import CalciumPage from './containers/CalciumPage'
-import CholestrolPage from './containers/CholestrolPage'
-import FatsPage from './containers/FatsPage'
+import infoPage from './containers/infoPage'
+
 // import MyListBeverages from './containers/MyListBeverages';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -22,8 +18,7 @@ export default function App() {
         <Stack.Screen name="overlay" component={Overlay} />
         <Stack.Screen name="HomePage" component={HomePage} /> 
         <Stack.Screen name="MyLists" component={MyLists} />
-        <Stack.Screen name="InfoPage" component={InfoPage} />
-        <Stack.Screen name="SugarPage" component={SugarPage} />
+        <Stack.Screen name="InfoPage" component={infoPage} />
         <Stack.Screen name="MyProfilePage" component={MyProfilePage} />
         {/* <Stack.Screen name="MyListBeverages" component={MyListBeverages} />   */}
       </Stack.Navigator>
