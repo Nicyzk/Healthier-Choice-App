522c3625edcfd035fb235fcf9fdbd61fe64e5bb4
nicholas <nicyzk@gmail.com>
2022-10-25 01:53:36 +0100

    nic

--- Backend/server.js
+++ Backend/server.js
@@ -7,4 +7,10 @@ app.use(express.json())
 
 app.use('/api', userRoutes);
 
+app.use('/', (req, res, next) => {
+    res.status(404).json({
+        errorMessage: "No endpoint found"
+    })
+})
+
 app.listen(port, () => console.log(`app listening on port ${port}`))
