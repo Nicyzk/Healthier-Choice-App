ead4e40006d4fae818a448c6c8a0cbc005481a59
lowzk <lowzhekai69@gmail.com>
2022-11-01 14:47:30 +0800

    Add png photos

--- frontend/consts/images.js
+++ frontend/consts/images.js
@@ -1,8 +1,24 @@
 const images = {
     1: require('../assets/100plus.png'),
+    2: require('../assets/100pluszero.png'),
+    3: require('../assets/brownrice.png'),
     4: require('../assets/7up.png'),
+    5: require('../assets/afiatcornoil.png'),
+    6: require('../assets/ayambrandbeans.png'),
     7: require('../assets/cocolifecoconutwater.png'),
+    8: require('../assets/elbrownrice.png'),
+    9: require('../assets/birdnest.png'),
+    10: require('../assets/naturalfruits.png'),
+    11: require('../assets/gardenpricksalmond.png'),
     12: require('../assets/goldeneaglerice.png'),
+    13: require('../assets/goldenbellsoya.png'),
+    14: require('../assets/heavenandearthgreentea.png'),
+    15: require('../assets/kikkoman.png'),
+    16: require('../assets/knorr.png'),
+    17: require('../assets/magnolialowfat.png'),
+    18: require('../assets/marigoldorange.png'),
+    19: require('../assets/natureloil.png'),
+    20: require('../assets/quakeroatmeal.png')
 }
 
 export default images
